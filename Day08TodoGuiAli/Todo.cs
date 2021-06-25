d1bb2630827bd3a8a74e8f292c71bf23027de998
alialaghbandrad <72175406+alialaghbandrad@users.noreply.github.com>
2021-06-25 18:38:07 -0400

    Add files via upload

---
+++ Day08TodoGuiAli/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08TodoGuiAli
+{
+    class Todo
+    {
+        String Task; // 1-100 characters, made up of uppercase and lowercase letters, digits, space, %_-(),./\ only
+        int Difficulty; // 1-5, as slider
+        DateTime DueDate; // year 1900-2100 both inclusive, use formatted field
+        public EStatus Status; // define an enumeration - one of: Pending, Done, Delegated - matches the ComboBox in GUI
+        public enum EStatus { Pending, Done, Delegated };
+    }
+}
