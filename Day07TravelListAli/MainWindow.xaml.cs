d8d01a3ef6198eaa2704e6b85b8b38b76d9830b6
alialaghbandrad <72175406+alialaghbandrad@users.noreply.github.com>
2021-06-25 18:37:40 -0400

    Add files via upload

---
+++ Day07TravelListAli/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day07TravelListAli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const string DataFileName = @"..\..\data.txt";
+
+        List<Trip> tripsList = new List<Trip>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDataFromFile();
+            lvTripList.ItemsSource = tripsList;
+        }
+
+        private void btAddTrip_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Get/Set the inputs:
+                string destination = tbDestination.Text;
+                string name = tbName.Text;
+                string passportNo = tbPassportNo.Text;
+                if (dpDeparture.SelectedDate == null || dpReturn.SelectedDate == null)
+                {
+                    MessageBox.Show(this, "Please select both departure and return dates", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                DateTime departureDate = dpDeparture.SelectedDate.Value;
+                DateTime returnDate = dpReturn.SelectedDate.Value;
+                //
+                Trip trip = new Trip(destination, name, passportNo, departureDate, returnDate); // ex InvalidDataException
+                tripsList.Add(trip);
+                lvTripList.Items.Refresh(); // inform ListView that data has changed   (version 1)
+                // lvTripList.Items.Add(trip);       (version 2)
+                // Clear the inputs
+                tbDestination.Clear();
+                tbName.Clear();
+                tbPassportNo.Clear();
+                dpDeparture.SelectedDate = null;
+                dpReturn.SelectedDate = null;
+            } catch (InvalidDataException ex)
+            {
+                MessageBox.Show(this, ex.Message, "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btSaveSelected_Click(object sender, RoutedEventArgs e)
+        {
+            var selItems = lvTripList.SelectedItems.Cast<Trip>().ToList();
+            if (selItems.Count == 0)
+            {
+                MessageBox.Show(this, "Please select some items first", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            // saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string fileName = saveFileDialog.FileName;
+                SaveDataToFile(fileName, selItems);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveDataToFile(DataFileName, tripsList);
+        }
+
+        private void SaveDataToFile(string fileName, List<Trip> tripsToSave)
+        {
+            try
+            {
+                List<string> linesList = new List<string>();
+                foreach (Trip t in tripsToSave)
+                {
+                    linesList.Add(t.ToDataString());
+                }
+                File.WriteAllLines(fileName, linesList);
+            }
+            catch (Exception ex) when (ex is IOException || ex is SystemException)
+            {
+                MessageBox.Show(this, ex.Message, "File IO error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LoadDataFromFile()
+        {
+            try
+            {
+                if (!File.Exists(DataFileName)) return; // do nothing if the file does not exist yet
+                string[] linesList = File.ReadAllLines(DataFileName);
+                foreach (string line in linesList)
+                {
+                    try
+                    {
+                        Trip trip = new Trip(line); // ex InvalidDataException
+                        tripsList.Add(trip);
+                    }
+                    catch (InvalidDataException ex)
+                    {
+                        MessageBox.Show(this, "Ignoring invalid line.\n" + ex.Message, "File data error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                File.WriteAllLines(@"..\..\data.csv", linesList);
+            }
+            catch (Exception ex) when (ex is IOException || ex is SystemException)
+            {
+                MessageBox.Show(this, ex.Message, "File IO error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void lvTripList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Day07TravelListAli/Trip.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day07TravelListAli
+{
+    public class InvalidDataException : Exception
+    {
+        public InvalidDataException(string msg) : base(msg) { }
+        public InvalidDataException(string msg, Exception inner) : base(msg, inner) { }
+    }
+
+    public class Trip
+    {
+
+        public Trip(string dataLine) // ex InvalidDataException
+        {
+            try
+            {
+                string[] data = dataLine.Split(';');
+                if (data.Length != 5)
+                {
+                    throw new InvalidDataException("Invalid number of elements in line");
+                }
+                Destination = data[0];
+                TravellerName = data[1];
+                TravellerPassport = data[2];
+                DateTime depDate = DateTime.ParseExact(data[3], "yyyy-MM-dd", CultureInfo.InvariantCulture); // ex
+                DateTime retDate = DateTime.ParseExact(data[4], "yyyy-MM-dd", CultureInfo.InvariantCulture); // ex
+                SetDepartureReturnDates(depDate, retDate);
+            }
+            catch (FormatException ex)
+            {
+                throw new InvalidDataException("Data format invalid in line", ex);
+            }
+        }
+
+        public Trip(string destination, string travellerName, string travellerPassport, DateTime departureDate, DateTime returnDate)
+        {
+            Destination = destination;
+            TravellerName = travellerName;
+            TravellerPassport = travellerPassport;
+            SetDepartureReturnDates(departureDate, returnDate);
+        }
+
+        private string _destination;
+        public string Destination
+        {
+            get
+            {
+                return _destination;
+            }
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z0-9 \.]{2,30}$"))
+                {
+                    throw new InvalidDataException("Destinatin must be 2-30 characters long");
+                }
+                _destination = value;
+            }
+        }
+
+        private string _travellerName;
+        public string TravellerName
+        {
+            get
+            {
+                return _travellerName;
+            }
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z0-9 \.]{2,30}$"))
+                {
+                    throw new InvalidDataException("Traveller name must be 2-30 characters long");
+                }
+                _travellerName = value;
+            }
+        }
+
+        private string _travellerPassport;
+        public string TravellerPassport
+        {
+            get
+            {
+                return _travellerPassport;
+            }
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[A-Z]{2}[0-9]{6}$"))
+                {
+                    throw new InvalidDataException("Passport must be in AA123456 format");
+                }
+                _travellerPassport = value;
+            }
+        }
+
+
+        private DateTime _departureDate, _returnDate;
+        public DateTime DepartureDate
+        {
+            get { return _departureDate; }
+        }
+        public DateTime ReturnDate
+        {
+            get { return _returnDate; }
+        }
+
+        public void SetDepartureReturnDates(DateTime dep, DateTime ret)
+        {
+            if (ret < dep)
+            {
+                throw new InvalidDataException("Return can't be before departure");
+            }
+            _departureDate = dep;
+            _returnDate = ret;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{TravellerName} ({TravellerPassport}) to {Destination} from {DepartureDate:d} to {ReturnDate:d}";
+        }
+
+        public string ToDataString()
+        {
+            return $"{Destination};{TravellerName};{TravellerPassport};{DepartureDate:yyyy-MM-dd};{ReturnDate:yyyy-MM-dd}";
+        }
+
+
+    }
+}
