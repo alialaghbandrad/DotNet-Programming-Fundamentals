362b060af9e6a36978c803d6a785651f216a00a8
alialaghbandrad <72175406+alialaghbandrad@users.noreply.github.com>
2021-06-25 18:38:35 -0400

    Add files via upload

---
+++ Day09FriendsCustDlgAli/AddEditDialog.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Day09FriendsCustDlgAli
+{
+    /// <summary>
+    /// Interaction logic for AddEditDialog.xaml
+    /// </summary>
+    public partial class AddEditDialog : Window
+    {
+        public AddEditDialog()
+        {
+            InitializeComponent();
+            if (name == null)
+            { // adding
+                btSave.Content = "Add";
+            }
+            else
+            { // editing
+                btSave.Content = "Update";
+                tbName.Text = name;
+            }
+        }
+
+        /*
+        private string _nameResult;
+        public string NameResult // read-only property
+        {
+            get { return _nameResult; }
+        } */
+
+        private void btSave_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbName.Text;
+            if (name.Length < 2 || name.Length > 20)
+            {
+                MessageBox.Show(this, "Name must be 2-20 characters long", "Input error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            // _nameResult = name;
+            DialogResult = true; // assing result and dismiss the dialog
+        }
+    }
+}
---
+++ Day09FriendsCustDlgAli/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day09FriendsCustDlgAli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> friendsList = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lvFriends.ItemsSource = friendsList;
+        }
+
+        private void miEditAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditDialog dlg = new AddEditDialog();
+            dlg.Owner = this;
+            if (dlg.ShowDialog() == true)
+            {
+                // string name = dlg.NameResult;
+                string name = dlg.tbName.Text;
+                friendsList.Add(name);
+                lvFriends.Items.Refresh();
+            }
+        }
+
+        private void lvFriends_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            string item = (string)lvFriends.SelectedItem; // possible multiple selection
+            if (item == null) return;
+            AddEditDialog dlg = new AddEditDialog(item);
+            dlg.Owner = this;
+            if (dlg.ShowDialog() == true)
+            {
+                int index = friendsList.IndexOf(item);
+                friendsList[index] = dlg.tbName.Text;
+                lvFriends.Items.Refresh();
+            }
+        }
+    }
+}
